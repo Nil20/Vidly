2b0640e61f78f6e4565cb3c9f9d9fca5fbb93817
Nil20 <nileeeem36@gmail.com>
2022-12-05 20:00:25 +0600

    adding basic routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Movies from './components/movies';
+import NotFound from './components/not-found';
 
 class App extends Component {
   render() {
     return (
       <main className='container'>
-        <Movies/>
+        <div className='content'>
+          <Switch>
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from='/movies' to='/' />
+            <Route path="/" exact component={Movies}/>
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
       </main>
     );
   }  
