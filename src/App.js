a4e773ac5842c0fa096f3fa16f675e093d0ca884
Nil20 <nileeeem36@gmail.com>
2022-12-06 15:10:59 +0600

    navigation bar addition with routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import './App.css';
-import Movies from './components/movies';
-import NotFound from './components/not-found';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import MovieForm from "./components/movieForm";
+import Rentals from "./components/rentals";
+import Movies from "./components/movies";
+import NotFound from "./components/not-found";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className='container'>
-        <div className='content'>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
           <Switch>
+            <Route path="/movie/:id" component={MovieForm} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
-            <Redirect from='/movies' to='/' />
-            <Route path="/" exact component={Movies}/>
+            <Route path="/movies" component={Movies} />
+            <Route path="/" exact component={Movies} />
             <Redirect to="/not-found" />
           </Switch>
-        </div>
-      </main>
+        </main>
+      </React.Fragment>
     );
-  }  
+  }
 }
 
 export default App;
--- src/components/not-found.jsx
+++ src/components/not-found.jsx
@@ -1,5 +1,5 @@
 const NotFound = () => {
-    return <p>404</p>;
+    return <h1>404 Not Found</h1>;
 }
  
 export default NotFound;
